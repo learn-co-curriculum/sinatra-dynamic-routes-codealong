b7b5ce9b240bf438bbca28c007660598d8195bbc
dfenjves <fenjves.daniel@gmail.com>
2015-09-15 10:21:56 -0400

    lesson in progress

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  # This is a sample static route.
+  get '/hello' do
+    "Hello World!"
+  end
+
+  # This is a sample dynamic route.
+  get "/hello/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
+
 end
