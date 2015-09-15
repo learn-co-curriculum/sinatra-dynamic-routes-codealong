6cf666f957ca59538f4b86e80df62faf59670a3f
Ian Candy <iancandysemail@gmail.com>
2015-09-15 13:01:01 -0400

    add solutioN

--- app.rb
+++ app.rb
@@ -23,13 +23,4 @@ class App < Sinatra::Base
   	"#{@product}"
   end
 
-  get "/goodbye/:name" do
-    @name = params[:name]
-    "Goodbye, #{@name}."
-  end
-
-  get "/multiply/:num1/:num2" do
-    @product = params[:num1].to_i * params[:num2].to_i
-    "#{@product}"
-  end
 end
